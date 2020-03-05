b0ca6ce3884f073f82901b834195dce4da4ac67a
Diogo_Ramos <diogoramos@pm.me>
2020-03-05 14:11:18 +0000

    inicio do backend

---
+++ Desenvolvimento/EasyTrip/easytrip/src/test/java/com/easytrip/easytrip/EasytripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easytrip.easytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
