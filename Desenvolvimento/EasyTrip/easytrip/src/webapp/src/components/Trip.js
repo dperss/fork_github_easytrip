e4b3627b6d20f7b5834ec968bbd5528c7dc190ab
ClaudiaAlves <a36737@alunos.ipb.pt>
2020-08-10 18:30:54 +0100

    inicio pag de viagem

--- Desenvolvimento/EasyTrip/easytrip/src/webapp/src/components/Trip.js
+++ Desenvolvimento/EasyTrip/easytrip/src/webapp/src/components/Trip.js
@@ -1,92 +1,94 @@
-import React, { Component } from 'react';
-import {
-    Tab,
-    Col,
-    Table,
-    Jumbotron,
-    Container, FormGroup,
-} from "react-bootstrap";
-import uuid from "uuid";
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Row, Col, Button, Jumbotron} from "react-bootstrap";
+import {DateRangePicker} from "react-dates";
+import Background from "../assets/images/imagem.png";
 
 
 
-const containerStyle = {
-  position: 'relative',
-  width: '100%',
-  height: '100%'
-}
-const style = {
-  width: '100%',
-  height: 500
-}
 
 
 export class Trip extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: null,
+            endDate: null
 
-
-fetchPlaces(mapProps, map) {
-  const {google} = mapProps;
-  const service = new google.maps.places.PlacesService(map);
-  // ...
-}
-
-    render(){
-var points = [
-    { lat: 42.02, lng: -77.01 },
-    { lat: 42.03, lng: -77.02 },
-    { lat: 41.03, lng: -77.04 },
-    { lat: 42.05, lng: -77.02 }
-]
-var bounds = new this.props.google.maps.LatLngBounds();
-for (var i = 0; i < points.length; i++) {
-  bounds.extend(points[i]);
-}
-
-    if (!this.props.loaded) {
-          return <div>Loading...</div>
         }
-
-        return(
-
-
-
-                <Container containerStyle={containerStyle}>
-                    <Row>
-                        <Col sm={4}>
-<div class="container">
-
-      </div>
+    }
 
 
 
-                        </Col>
-                        <Col sm={8} style={style}>
+    render() {
 
-                        <Map
-                                google={this.props.google}
+        const  jumbstyle = {
+            backgroundImage: `url(${Background})`,
+            backgroundPosition: 'top',
+            backgroundSize: 'cover',
 
-                                initialCenter={{
-                                    lat: 42.39,
-                                    lng: -72.52
-                                }}
-                                bounds={bounds}>
-                            </Map>
-                        </Col>
-
-                      </Row>
+        };
 
 
 
-                </Container>
+        return (
+            <div>
+
+                        <Jumbotron style = {jumbstyle}>
+                            <Row>
+                                <Col>
+                                    <label> De onde:</label>
+                                    <input className={"form-control"}
+                                           type={"text"}
+                                    />
+                                </Col>
+                                <Col>
+                                    <label> Para onde:</label>
+                                    <input className={"form-control"}
+                                           type={"text"}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row style = {{justifyContent: "center", marginTop: "10px"}}>
+
+                                <DateRangePicker
+                                    startDate = {this.state.startDate}
+                                    startDateId = "your_unique_start_date_id"
+                                    endDate = {this.state.endDate}
+                                    endDateId = "your_unique_end_date_id"
+                                    onDatesChange = {({ startDate, endDate }) => this.setState({ startDate, endDate })}
+                                    focusedInput = {this.state.focusedInput}
+                                    onFocusChange = {focusedInput => this.setState({ focusedInput })}
+
+                                />
+
+                            </Row>
+                            <Row style = {{justifyContent: "center", marginTop: "10px"}}>
+                                <Button variant="info" > Viajar </Button>
+                            </Row>
+                        </Jumbotron>
 
 
+
+                <Row>
+
+
+                </Row>
+
+
+
+                <Row>
+
+                    <h5>ALOJAMENTOS</h5>
+
+
+
+                </Row>
+
+                <Row>
+                    <h5>TRANSPORTES</h5>
+                </Row>
+            </div>
         );
-
-
     }
 }
-
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCTlmafkvB17b5ZsF884AusohJ9CW1aReg'
-})(Trip)
